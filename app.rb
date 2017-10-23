88e2abcd3c056c45c65b910f00a20a0a6d5ee5c3
Robby Ranshous <rranshous@gmail.com>
2017-10-22 22:48:58 -0400

    refactor

--- app.rb
+++ app.rb
@@ -1,94 +1,18 @@
 #!/usr/bin/env ruby
 
-require 'rss'
-require 'open-uri'
-require 'base64'
-require 'json'
 require 'pry'
+require_relative 'objs'
 
 OUT_BASE = File.absolute_path('./data')
+SUBREDDIT_NAME = 'PropagandaPosters'
 
-def log msg
-  STDERR.write("#{msg}\n")
-end
+feed_scanner    = FeedScanner.new(subreddit_name: SUBREDDIT_NAME)
+post_scanner    = PostScanner.new(feed_scanner: feed_scanner)
+image_scanner   = ImageScanner.new(post_scanner: post_scanner)
+image_writer    = ImageWriter.new(out_dir: OUT_BASE)
+metadata_writer = MetadataWriter.new(out_dir: OUT_BASE)
 
-def open_slow url, &blk
-  req_args = {
-    "User-agent" => "https://github.com/rranshous/rip_reddit_images"
-  }
-  log "opening: #{url}"
-  open(url, req_args) do |*args|
-    blk.call(*args)
-  end
-
-rescue URI::InvalidURIError => ex
-  log "invalid url: #{ex}"
-
-rescue OpenURI::HTTPError => ex
-  log "ex: #{ex}"
-  if ex.message[/429 Too Many Requests/]
-    log "sleeping"
-    sleep 10
-    open_slow url, &blk
-  end
-end
-
-def file_name post_url, image_url
-  ext = image_url.split('.').last
-  "#{Base64.urlsafe_encode64(post_url)}.#{ext}"
-end
-
-# count, after
-last = ''
-1000.times do |page|
-  log "page: #{page+1}"
-  page_url = "https://www.reddit.com/r/PropagandaPosters/.rss?count=25&after=#{last}"
-  open_slow(page_url) do |rss|
-    feed = RSS::Parser.parse(rss)
-    log "Subreddit: #{feed.title.content}"
-    feed.items.each do |item|
-      last = item.id.content
-      post_html_url = item.link.href
-      post_url = URI.join(item.link.href, '.rss') rescue nil
-      post_url ||= item.link.href + '.rss'
-      open_slow(post_url) do |post_rss|
-        post_feed = RSS::Parser.parse(post_rss)
-        title = post_feed.title.content
-        log "Post: #{title}"
-        content = post_feed.entry.content.content
-        img_url = content[/https:\/\/i.redd\.it.*?jpg/]
-        img_url ||= content[/https:\/\/i.imgur.com.*?.jpg/]
-        img_url ||= content.match(/(http.*?jpg).*?(http.*?jpg)/)[2][/href.*/][/https.*/] rescue nil
-        if img_url
-          image_name = file_name(post_html_url.to_s, img_url)
-          out_path = Pathname.new(File.join(OUT_BASE, image_name))
-          meta_out_path = out_path.to_s + '.meta.json'
-          if out_path.exist?
-            log "skipping, exists: #{out_path}"
-          else
-            log "downloading image: #{img_url}"
-            data_size = nil
-            open_slow(img_url) do |img_data|
-              log "writing image: #{out_path}"
-              File.open(out_path, 'wb') do |fh|
-                data = img_data.read
-                data_size = data.size
-                fh.write data
-              end
-            end
-            log "writing meta: #{meta_out_path}"
-            File.open(meta_out_path, 'w') do |fh|
-              fh.write(JSON.dump({
-                original_post_url: post_html_url,
-                image_url: img_url,
-                image_name: image_name,
-                image_data_size: data_size,
-              }))
-            end
-          end
-        end
-      end
-    end
-  end
-
+image_scanner.each do |image|
+  image_writer.write image
+  metadata_writer.write image
 end
---
+++ objs.rb
@@ -0,0 +1,251 @@
+require 'open-uri'
+require 'base64'
+require 'rss'
+require 'json'
+
+def log msg
+  STDERR.write("#{msg}\n")
+end
+
+module HttpOpener
+  def open_slow url, &blk
+    req_args = {
+      "User-agent" => "https://github.com/rranshous/rip_reddit_images"
+    }
+    log "opening: #{url}"
+    open(url, req_args) do |*args|
+      blk.call(*args)
+    end
+
+  rescue URI::InvalidURIError => ex
+    log "invalid url: #{ex}"
+    nil
+
+  rescue OpenURI::HTTPError => ex
+    log "ex: #{ex}"
+    if ex.message[/429 Too Many Requests/]
+      log "sleeping"
+      sleep 10
+      open_slow url, &blk
+    end
+    nil
+  end
+end
+
+class Post
+  include HttpOpener
+  attr_accessor :url, :name
+
+  def self.from_feed feed
+    feed.items.map do |item|
+      post = new
+      post.url = item.link.href
+      post.name = item.title.content
+      post
+    end
+  end
+
+  def content
+    feed.entry.content.content if !feed.nil?
+  end
+
+  def title
+    feed.title.conten if !feed.nil?
+  end
+
+  def feed
+    return @parsed_feed if @parsed_feed
+    @parsed_feed = open_slow(url + '.rss') do |data|
+      if data
+        RSS::Parser.parse(data)
+      else
+        nil
+      end
+    end
+  end
+end
+
+class ImageWriter
+  attr_accessor :out_dir
+
+  def initialize out_dir: './data'
+    self.out_dir = out_dir
+  end
+
+  def write image
+    data = image.data
+    save_path = save_path_for image
+    if data.nil?
+      log "[nildata] skipping data: #{image}"
+    elsif data.length == 0
+      log "[nodata] skipping data: #{image}"
+    elsif save_path.exist?
+      log "[already-downloaded] skipping data: #{image}"
+    else
+      log "writing data: #{image}"
+      write_data image, data
+      image.save_path = save_path
+    end
+  end
+
+  def save_path_for image
+    Pathname.new File.join(out_dir, image.save_name)
+  end
+
+  private
+
+  def write_data image, data
+    File.open(save_path_for(image), 'wb') do |fh|
+      fh.write data
+    end
+  end
+end
+
+class MetadataWriter
+  attr_accessor :out_dir
+
+  def initialize out_dir: './data'
+    self.out_dir = out_dir
+  end
+
+  def write image
+    if !image.saved?
+      log "[notsaved] skipping meta: #{image}"
+    else
+      log "writing meta: #{image}"
+      write_data image
+    end
+  end
+
+  def save_path_for image
+    Pathname.new(image.save_path.to_s + '.meta.json')
+  end
+
+  private
+
+  def write_data image
+    File.open(save_path_for(image), 'wb') do |fh|
+      fh.write image.to_json
+    end
+  end
+
+  def ext
+    '.meta.json'
+  end
+end
+
+class Image
+  include HttpOpener
+  attr_accessor :url, :name, :ext, :post_name, :post_url, :save_path, :data_size
+
+  def self.from_post post
+    return nil if post.content.nil?
+    url = PostParser.image_url_from_content post.content
+    return nil if url.nil?
+
+    image = new
+    image.url       = url
+    image.name      = image.url.split('/').last
+    image.ext       = image.name.split('.').last
+    image.post_name = post.name
+    image.post_url  = post.url
+    image
+  end
+
+  def data
+    _data = open_slow(url) { |image_data| image_data.read }
+    self.data_size = _data.length if !_data.nil?
+    _data
+  end
+
+  def save_name
+    "#{Base64.urlsafe_encode64(post_url)}.#{ext}"
+  end
+
+  def saved?
+    !self.save_path.nil?
+  end
+
+  def to_h
+    { post_url: post_url, url: url, name: name, data_size: data_size }
+  end
+
+  def to_json
+    to_h.to_json
+  end
+end
+
+module PostParser
+  def self.image_url_from_content content
+    url = content[/https:\/\/i.redd\.it.*?jpg/]
+    url ||= content[/https:\/\/i.imgur.com.*?.jpg/]
+    url ||= content.match(/(http.*?jpg).*?(http.*?jpg)/)[2][/href.*/][/https.*/] rescue nil
+    url
+  end
+end
+
+class ImageScanner
+  attr_accessor :post_scanner
+
+  def initialize post_scanner: nil
+    self.post_scanner = post_scanner
+  end
+
+  def each
+    post_scanner.each do |post|
+      image = Image.from_post post
+      yield image if !image.nil?
+    end
+  end
+end
+
+class PostScanner
+  attr_accessor :feed_scanner
+
+  def initialize feed_scanner: nil
+    self.feed_scanner = feed_scanner
+  end
+
+  def each
+    feed_scanner.each do |page_feed|
+      Post.from_feed(page_feed).each do |post|
+        yield post
+      end
+    end
+  end
+end
+
+class FeedScanner
+  include HttpOpener
+  attr_accessor :subreddit_name, :last_id
+
+  def initialize subreddit_name: nil
+    self.subreddit_name = subreddit_name
+  end
+
+  def each
+    each_page do
+      feed = get_page_feed
+      yield feed
+      self.last_id = feed.items.last.id.content
+    end
+  end
+
+  def page_url
+    "https://www.reddit.com/r/#{subreddit_name}/.rss?count=25&after=#{last_id}"
+  end
+
+  def feed_url
+    "#{page_url}.rss"
+  end
+
+  private
+
+  def get_page_feed
+    open_slow(feed_url) { |data| RSS::Parser.parse data }
+  end
+
+  def each_page
+    1000.times { yield }
+  end
+end
